06c6daae67f45b1cfe9fbb4f803cec5a4120deee
gvaireth <pzajaczkowski@gmail.com>
2016-08-18 17:17:14 +0200

    unit tests experiments

--- src/test/java/org/gvaireth/goeuro/server/RouteFinderImplTest.java
+++ src/test/java/org/gvaireth/goeuro/server/RouteFinderImplTest.java
@@ -6,19 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ComponentScan({ "org.gvaireth.goeuro.server" })
-public class RoutesDaoImplTest {
+@ContextConfiguration
+@ComponentScan({ "org.gvaireth.goeuro.server", "org.gvaireth.goeuro.core" })
+public class RouteFinderImplTest {
 
 	@Autowired
-	private RoutesDao service;
+	private RouteFinderService service;
 
 	@Test
 	public void test() {
-		service.buildRoutes();
+		service.directRouteExists(1, 2);
 	}
 
 }
